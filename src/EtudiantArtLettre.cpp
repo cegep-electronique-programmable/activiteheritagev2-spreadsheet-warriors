6bbe766c683824009da60c0df419359d4a85b937
Godson702 <e2378353@cmaisonneuve.qc.ca>
2025-05-13 20:27:26 -0400

    hello

--- src/EtudiantArtLettre.cpp
+++ src/EtudiantArtLettre.cpp
@@ -1,21 +1,65 @@
-#include "Etudiant.h"
-#include "EtudianArtLettre.h"
+/*
+ * Nom de fichier 
+ *      Etudiant.cpp
+ * 
+ * Sommaire
+ *      Fichier source pour la classe Etudiant.
+ * 
+ * Auteur 
+ *      Maxime Champagne
+ *  
+ */
 
-class EtudiantArtLettre : public Etudiant {
-    int numeroMatricule;
-    int moyenne;
-	
-public:
-    void setMatriculeAl(int);
-    
-};
+#include "EtudiantArtLettre.h"
 
-void EtudiantArtLettre::setMatriculeAl(int nouveauMatricule){
+//Définition de la méthode setMatricule de la classe, permet de modifier le matricule de l'étudiant
+void Etudiant::setMatricule(int nouveauMatricule){
+    //Le numero de matricule doit necessairement avoir 7 chiffres
+    if ((nouveauMatricule >= 1000000) && (nouveauMatricule <= 9999999)){
+        this->numeroMatricule = nouveauMatricule;    
+    }
+    else{
+        this->numeroMatricule = 0; //Le matricule est mis à 0 si le matricule n'est pas valide
+    }
+}
+
+
+
+int Etudiant::getMatricule(void){
+    return (this->numeroMatricule);
+}
+
+void Etudiant::setMoyenne(int nouvelleMoyenne){
+    if (nouvelleMoyenne <= 100){
+        this->moyenne = nouvelleMoyenne;
+    }
+    else{
+        this->moyenne = 100;
+    }
+}
+
+int Etudiant::getMoyenne(void){
+    return (this->moyenne);
+}
+
+
+
+void EtudiantArtLettre::setMatricule(int nouveauMatricule){
     //Le numero de matricule doit necessairement avoir 7 chiffres
     if ((nouveauMatricule >= 7000000) && (nouveauMatricule <= 7999999)){
         this->numeroMatricule = nouveauMatricule;    
     }
     else{
         this->numeroMatricule = 0; //Le matricule est mis à 0 si le matricule n'est pas valide
+    }
+}
+
+void EtudiantArtLettre::NbrLivresLus(int nouveauNbrLivresLus){
+    //Le numero de matricule doit necessairement avoir 7 chiffres
+    if ((nouveauNbrLivresLus >= 0000) && (nouveauNbrLivresLus <= 9999)){
+        this->nbrLivresLus = nouveauNbrLivresLus;    
+    }
+    else{
+        this->nbrLivresLus = 0; //Le matricule est mis à 0 si le matricule n'est pas valide
     }
 }
